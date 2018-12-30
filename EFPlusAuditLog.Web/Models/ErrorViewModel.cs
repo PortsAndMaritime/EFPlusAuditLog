8a70e2a0760d0a460f5e15ee5659706dcff8a1b9
hoseinipeyrov <mir.hadi.peyrov@gmail.com>
2018-12-30 10:07:35 +0330

    Init sln

---
+++ EFPlusAuditLog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EFPlusAuditLog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
